5f09e7308278662a751c3ce6c32b202d8ebe24ea
poojapdalvi26-hub <poojapdalvi26@gmail.com>
2026-06-20 16:07:34 +0530

    modified EG

--- src/assignment/EG.java
+++ src/assignment/EG.java
@@ -3,5 +3,7 @@ package assignment;
 public class EG {
     public static void main(String[] args) {
         System.out.println("Testing");
+        System.out.println("Testing2");
+        System.out.println("3445");
     }
 }
